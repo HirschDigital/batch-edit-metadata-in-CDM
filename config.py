51f30bd419d6fe65e75cd5d0d76002638fb77c6d
HirschDigital <slong@mfah.org>
2025-06-16 12:54:03 -0500

    change it

---
+++ config.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+cdm = {
+    'url': 'http://server17480.contentdm.oclc.org:8888',
+    'username': 'SarahLong',
+    'password': 'ep9riseSkinwalker',
+    'license': 'R7WWM-LE5RB-HPYRT-RZJ53',
+}
